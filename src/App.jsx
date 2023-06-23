2cd3ca4f669a284c8cd960cb860f4dc51a619232
Sebabatso Mashego <sebabatsomashego.dev@gmail.com>
2023-06-24 01:54:31 +0200

    added continuous rotation within set range to keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { Perf } from 'r3f-perf';
 import Scene from './Scene';
 
 export default function App() {
@@ -9,6 +10,15 @@ export default function App() {
             <Canvas shadows flat linear>
                 <Scene />
                 <OrbitControls enablePan={false} />
+                <Perf
+                    position={'top-left'}
+                    style={{
+                        marginTop: '10px',
+                        marginLeft: '15px',
+                    }}
+                />
+                <axesHelper args={[5000]} />
+                <gridHelper scale={400} />
             </Canvas>
         </Suspense>
     );
