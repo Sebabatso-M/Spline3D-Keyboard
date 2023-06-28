e72a0732ac4296bc858323200551354da940a252
Sebabatso Mashego <sebabatsomashego.dev@gmail.com>
2023-06-29 00:47:06 +0200

    added autorotate controls to model

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
@@ -8,17 +8,41 @@ import Scene from './Scene';
 import { useState } from 'react';
 
 export default function App() {
-    // const [debugMode, setDebugMode] = useState(false)
+    const [enableRotControl, setEnableRotControl] = useState(false);
 
-    const { debug } = useControls({
+    const { debug, autoRotateSpeed } = useControls({
         debug: false,
+        autoRotate: {
+            value: enableRotControl,
+            onChange: (value) => {
+                setEnableRotControl(value);
+                console.log(enableRotControl);
+            },
+        },
+        autoRotateSpeed: {
+            value: 2.0,
+            min: 2.0,
+            max: 10,
+            disabled: enableRotControl,
+        },
     });
 
     return (
         <Suspense fallback={null}>
             <Canvas shadows flat linear>
                 <Scene />
-                <OrbitControls enablePan={false} />
+                {/* <Leva collapsed={true} /> */}
+                <OrbitControls
+                    // left and right
+                    /* minAzimuthAngle={-Math.PI / 6}
+                    maxAzimuthAngle={Math.PI / 6} */
+                    // up and down
+                    minPolarAngle={Math.PI / 4} // up limit
+                    maxPolarAngle={Math.PI - Math.PI / 2.5} // down limit
+                    autoRotate={enableRotControl}
+                    autoRotateSpeed={autoRotateSpeed}
+                />
+                {/* <OrbitControls enablePan={false} /> */}
 
                 {debug && (
                     <>
